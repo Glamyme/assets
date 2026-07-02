530c29b3fb6f4728ec918b99b6140194fb6d9d88
neoconejo <neoconejo2005@gmail.com>
2026-07-02 17:20:28 -0500

    Create golden_and-ruby_essences.js

---
+++ system/Essences/golden_and-ruby_essences.js
@@ -0,0 +1,26 @@
+{
+  "effects": {
+    "golden_essences": {
+      "id": "fx-gold-01",
+      "name": "Perfume Dorado Celestial",
+      "particle_count": 55,
+      "particle_color": "rgba(255, 215, 0, 0.9)",
+      "orbit_radius_x": 105,
+      "orbit_radius_y": 30,
+      "orbit_speed": 0.02,
+      "vertical_float_speed": -0.7,
+      "max_blur": 4
+    },
+    "ruby_essences": {
+      "id": "fx-ruby-01",
+      "name": "Perfume Rubí Carmesí",
+      "particle_count": 75,
+      "particle_color": "rgba(255, 0, 85, 0.9)",
+      "orbit_radius_x": 120,
+      "orbit_radius_y": 40,
+      "orbit_speed": 0.028,
+      "vertical_float_speed": -1.1,
+      "max_blur": 6
+    }
+  }
+}
